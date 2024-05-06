d9382aa25f00ab9aa772c56d9e71fa1a088e15b2
Ali Hassan <168643012+AliHassan-006@users.noreply.github.com>
2024-05-06 19:46:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+
+
+const inputBox=document.getElementById("input-box");
+const listContainer=document.getElementById("list-container");
+
+function addTask(){
+
+    if(inputBox==" "){
+
+        alert("Please Write some text...!")
+    }
+    else{
+    let li= document.createElement("li")
+
+    li.innerHTML=inputBox.value;
+    listContainer.appendChild(li);
+
+    let span=document.createElement("span")
+    span.innerHTML="\u00d7"
+
+    li.appendChild(span);
+
+    }
+    inputBox.value="";
+    SaveData()
+
+
+}
+
+
+listContainer.addEventListener("click", function(e){
+
+
+if (e.target.tagName==="LI"){
+
+    e.target.classList.toggle("checked");
+    SaveData()
+  
+}
+else if( e.target.tagName==="SPAN"){
+    e.target.parentElement.remove();
+    SaveData()
+   }
+        
+
+
+
+}, false);
+
+function SaveData(){
+
+localStorage.setItem("data",listContainer.innerHTML);
+
+}
+
+ function showTask(){
+
+listContainer.innerHTML=localStorage.getItem("data");
+
+
+ }
+ showTask();
